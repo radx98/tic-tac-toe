e8620631d76acf7d1168510a2fa9f44d53d1fbfc
Boris Ostanin <radx98@gmail.com>
2025-09-23 17:11:20 -0400

    move-handler-and-win-check-added

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -1,19 +1,32 @@
 import { useState } from 'react'
 import './App.css'
-import {GameState, initialState} from './tictactoe'
+import {type GameState, initialState, makeMove} from './tictactoe'
 
 export default function App() {
   const [gameState, setGameState] = useState<GameState>(initialState)
-  
-  function makeMove(prev, index) {
-    console.log(prev)
-    console.log(index)
+
+  function showStatus() {
+    if (gameState.winner != null && gameState.winner != 'draw') {
+      return `${gameState.winner} won!`
+    } else if (gameState.winner === 'draw') {
+      return "That's a draw :("
+    } else {
+      return `${gameState.currentPlayer}'s turn...`
+    }
+  }
+
+  function cellClicked(index: number) {
+    setGameState(makeMove(gameState, index))
   }
 
   return <>
     <h1>TIC-TAC-TOE</h1>
+    <h3>{showStatus()}</h3>
     <div className='grid'>
-      {gameState.board.map((cell, index) => <button key={index} onClick={() => makeMove(gameState, index)}>{cell}</button>)}
+      {gameState.board.map((cell, index) =>
+        <button key={index} onClick={() => cellClicked(index)}>
+          {cell}
+        </button>)}
     </div>
   </>
 }
--- tic-tac-toe/src/tictactoe.ts
+++ tic-tac-toe/src/tictactoe.ts
@@ -3,13 +3,48 @@ type Cell = Player | null
 type Board = [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell]
 
 export type GameState = {
-    currentMove: Player;
-    winner: Player | null
+    currentPlayer: Player;
+    winner: Player | null | 'draw';
     board: Board;
 }
 
 export const initialState: GameState = {
-    currentMove: 'X',
+    currentPlayer: 'X',
     winner: null,
     board: [null, null, null, null, null, null, null, null, null]
+}
+
+export function makeMove(prev: GameState, index: number) {
+    const newState = structuredClone(prev)
+    const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+    //add symbol to the cell + switch current player
+    if (prev.board[index] === null) {
+        newState.board[index] = prev.currentPlayer
+        newState.currentPlayer = prev.currentPlayer === 'X' ? 'O' : 'X'
+    }
+
+    //win check
+    for (let i = 0; i < winCombos.length; i++) {
+        const [a, b, c] = winCombos[i]
+        if (newState.board[a] === newState.board[b] && newState.board[a] === newState.board[c] && newState.board[a] != null) {
+                newState.winner = prev.currentPlayer
+        }
+    }
+
+    //draw check
+    let draw = true
+    for (let i = 0; i < 9; i++) {
+        if (newState.board[i] === null || newState.winner === null) {
+            draw = false
+            break
+        }
+    }
+    if (draw === true) {
+        newState.winner = 'draw'
+    }
+
+    console.log(newState)
+
+    return newState  
 }
